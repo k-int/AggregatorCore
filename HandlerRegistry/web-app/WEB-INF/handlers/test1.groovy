0eb6964c7a85da87135d95ccbca66819eb50f89f
Ian Ibbotson <ianibbo@googlemail.com>
2011-10-11 12:09:36 +0100

    Added test1.groovy class

---
+++ HandlerRegistry/web-app/WEB-INF/handlers/test1.groovy
@@ -0,0 +1,17 @@
+
+class test1 {
+  
+  def getHandlerName() {
+    "HelloWorld"
+  }
+
+  def getPreconditions() {
+    [
+      "some.pre.condition"
+    ]
+  }
+
+  def process() {
+    println "This is the code......."
+  }
+}
