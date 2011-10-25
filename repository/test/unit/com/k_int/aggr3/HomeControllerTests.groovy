16f017061b4350ecccc220d7f6022539fb731de4
Ian Ibbotson <ianibbo@googlemail.com>
2011-10-25 15:13:25 +0100

    Added home controller tests

---
+++ repository/test/unit/com/k_int/aggr3/HomeControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.k_int.aggr3
+
+import grails.test.*
+
+class HomeControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
