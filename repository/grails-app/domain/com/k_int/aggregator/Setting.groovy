9d5c752ccde83c2adade63b1cbd16b43aea1a37a
Ian Ibbotson <ianibbo@googlemail.com>
2011-10-26 16:38:05 +0100

    Added Setting Domain class

---
+++ repository/grails-app/domain/com/k_int/aggregator/Setting.groovy
@@ -0,0 +1,10 @@
+package com.k_int.aggregator
+
+class Setting {
+
+  String stKey
+  String stValue
+
+  static constraints = {
+  }
+}
---
+++ repository/test/unit/com/k_int/aggregator/SettingTests.groovy
@@ -0,0 +1,17 @@
+package com.k_int.aggregator
+
+import grails.test.*
+
+class SettingTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
