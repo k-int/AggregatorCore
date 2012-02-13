1e4eb416e7e6aa5d53add3c0c69297b836038a59
ianibo <ianibbo@googlemail.com>
2012-02-13 13:41:59 +0000

    More work on terminology service

--- repository/grails-app/conf/spring/resources.groovy
+++ repository/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,12 @@
 // Place your Spring DSL code here
 beans = {
+
+  // Attempting to use the terminology for some of our messages..
+  // We create our new message source refencing the old file based source.
+  messageSource(com.k_int.aggregator.TerminologyServiceMessageSource) {
+    messageBundleMessageSource = ref("messageBundleMessageSource")
+  }    
+  messageBundleMessageSource(org.codehaus.groovy.grails.context.support.PluginAwareResourceBundleMessageSource) {
+    basenames = "WEB-INF/grails-app/i18n/messages"
+  }
 }
--- repository/grails-app/services/com/k_int/aggregator/TerminologyServiceMessageSource.groovy
+++ repository/grails-app/services/com/k_int/aggregator/TerminologyServiceMessageSource.groovy
@@ -1,10 +1,26 @@
 package com.k_int.aggregator
 
 import org.springframework.context.support .*;
+import java.text.MessageFormat;
+
+// Largely based on code from http://efreedom.com/Question/1-8100312/Grails-I18n-Database-Default-Back-File
 
 class TerminologyServiceMessageSource extends AbstractMessageSource {
 
+  def messageBundleMessageSource
+
   protected MessageFormat resolveCode(String code, Locale locale) { 
+    
+    // Message msg = Message.findByCodeAndLocale(code, locale)
+    def format
+    // if(msg) {
+    //   format = new MessageFormat(msg.text, msg.locale)
+    // }
+    // else {
+      format = messageBundleMessageSource.resolveCode(code, locale)
+    // }
+    return format;
+
     // Message msg = Message.findByCodeAndLocale(code, locale)
     // def format
     // if(msg) {
@@ -14,6 +30,5 @@ class TerminologyServiceMessageSource extends AbstractMessageSource {
     //   format = new MessageFormat(code, locale )   
     // }
     // return format;
-    null;
   }
 }
