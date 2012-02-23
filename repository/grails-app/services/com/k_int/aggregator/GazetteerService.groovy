12e202aa598d3539ea97e16a8bfe08e03429bd43
ianibo <ianibbo@googlemail.com>
2012-02-23 16:16:17 +0000

    Work on gaz service

--- repository/grails-app/services/com/k_int/aggregator/GazetteerService.groovy
+++ repository/grails-app/services/com/k_int/aggregator/GazetteerService.groovy
@@ -82,22 +82,22 @@ class GazetteerService {
 
     println("Search for ${qry}");
 
-    def search_closure = {
+    def res = esclient.search {
+      indices "gaz"
       source {
         from = 0
         size = 10
         query {
           query_string (query: qry)
         }
-        sort {
-          pref {
-            order = 'desc'
-          }
-        }
+        sort = [
+          pref : [
+            order : 'desc'
+          ]
+        ]
       }
     }
 
-    def res = esclient.search(search_closure)
     println "Search returned $res.response.hits.totalHits total hits"
     res
 
