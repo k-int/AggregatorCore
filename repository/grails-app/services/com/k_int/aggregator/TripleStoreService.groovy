d8f230df715c75cf051c68466fdea69fb7e2d813
Ian Ibbotson <ianibbo@googlemail.com>
2012-02-29 22:31:41 +0000

    Added first attempt at triple store service

---
+++ repository/grails-app/services/com/k_int/aggregator/TripleStoreService.groovy
@@ -0,0 +1,59 @@
+package com.k_int.aggregator
+
+import groovy.util.slurpersupport.GPathResult
+import static groovyx.net.http.ContentType.URLENC
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+import groovyx.net.http.*
+import org.apache.http.entity.mime.*
+import org.apache.http.entity.mime.content.*
+import java.nio.charset.Charset
+import grails.converters.*
+import java.security.MessageDigest
+
+
+class TripleStoreService {
+
+  def fourstore_endpoint = null;
+  
+  @javax.annotation.PostConstruct
+  def init() {
+    log.debug("init...");
+    fourstore_endpoint = new HTTPBuilder( 'http://localhost:9000/' )
+    // fourstore_endpoint.auth.basic feed_definition.target.identity, feed_definition.target.credentials
+    // this.updateURL = new URL(baseurl + "/data/");
+  }
+
+  def update(graph, graph_uri, mimetype) { // throws MalformedURLException, ProtocolException, IOException {
+    fourstore_endpoint.request(POST) { request ->
+      // requestContentType = 'multipart/form-data'
+      requestContentType = 'application/x-www-form-urlencoded'
+
+      uri.path = 'data/'
+
+      body =  [ 
+        'mime-type' : 'mimetype' , 
+        'graph' : URLEncoder.encode(graph_uri, "UTF-8"),
+        'data' : URLEncoder.encode(graph, "UTF-8")
+      ] 
+
+      response.success = { resp, data ->
+        log.debug("response status: ${resp.statusLine} ${data}")
+      }
+
+      response.failure = { resp ->
+        log.error("Failure - ${resp}");
+      }
+    }
+
+    log.debug("Complete");
+  }
+
+  def removeGraph(graph_uri) {
+    // URL delete_url = new URL(baseurl + "/data/"+uri);
+    // HttpURLConnection connection = (HttpURLConnection) this.updateURL.openConnection();
+    // connection.setDoOutput(true);
+    // connection.setRequestMethod("DELETE");
+    // connection.connect();
+  }
+}
